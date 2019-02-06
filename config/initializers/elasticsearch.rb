9a552f3b64aebe55f750f1f1474573d7f6e85a37
Maxim Polunin <0x0badc0de@users.noreply.github.com>
2019-02-07 01:49:29 +0300

    HOST -> PORT

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,1 +1,1 @@
-Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV.fetch('APP_ELASTICSEARCH_HOST', 'elastic'), port: ENV.fetch('APP_ELASTICSEARCH_HOST', 9200).to_i, log: true
+Elasticsearch::Model.client = Elasticsearch::Client.new host: ENV.fetch('APP_ELASTICSEARCH_HOST', 'elastic'), port: ENV.fetch('APP_ELASTICSEARCH_PORT', 9200).to_i, log: true
